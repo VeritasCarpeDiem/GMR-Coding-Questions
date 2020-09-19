ffb7ee8abdc012abc671abeb35e4b135899aed32
Jin Kim <jinhong0425@gmail.com>
2020-09-18 21:19:11 -0700

    finished DS-10 question

--- CodingRiddles/Program.cs
+++ CodingRiddles/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace ConsoleApp1
 {
@@ -67,16 +68,113 @@ namespace ConsoleApp1
             return true;
         }
 
-
-        static void Main(string[] args)
+        public class Node
         {
-            //PrintTriangle();
 
-            // int number = ParseInt("420"); //int.Parse(userInput);
+            public Node Left { get; set; }
+            public Node Right { get; set; }
 
-            //Console.WriteLine(number);
 
-            Console.WriteLine(IsStringUnique("Jin"));
         }
+        static public int MakeTreetIteratively(int []arr, int num)
+        {
+            int low = 0;
+            int high = arr.Length - 1;
+            
+            while(low <= high)
+            {
+                int mid = (low + high) / 2; //arr={1,2,3,4,5} num=5
+
+                if(num ==arr[mid])
+                {
+                    return arr[mid];
+                }
+                if(num>arr[mid])
+                {
+                    low = mid + 1;
+                }
+                else //num<arr[mid]
+                {
+                    high = mid - 1;
+                }
+            }
+            return -1;
+        }
+        class SimpleTree
+        {
+
+            public int Value { get; set; }
+            public SimpleTree Left { get; set; }
+            public SimpleTree Right { get; set; }
+
+            public static SimpleTree Root { get; set; }
+            public static SimpleTree MakeTreeRecursively(int[] numbers, int low, int high) //binary search 
+            {
+                //Inorder traversal for sorted array
+                //int low = 0;
+                //int high = numbers.Length - 1;
+
+                if (low > high) //base case
+                {
+                    return null;
+                }
+                int mid = (low + high) / 2;//truncates the 0.5 if even # of elements in array
+
+                SimpleTree root = new SimpleTree() { Value = numbers[mid] };//set Root equal to mid element of array
+
+                root.Left = MakeTreeRecursively(numbers,low, mid-1);
+                root.Right = MakeTreeRecursively(numbers, mid+1, high);
+
+
+                return root;
+
+            }
+  
+            public static List<int> PreOrder()
+            {
+                List<int> ReturnPath = new List<int>();
+                SimpleTree current = Root;
+
+                HelperPreOrder(ReturnPath, current);
+                
+                return ReturnPath;
+                void HelperPreOrder(List<int> ReturnPath,SimpleTree current)
+                {
+                    ReturnPath.Add(current.Value);
+                    if (current.Left != null)
+                    {
+                        HelperPreOrder(ReturnPath, current.Left);
+                    }
+                    if (current.Right != null)
+                    {
+                        HelperPreOrder(ReturnPath, current.Right);
+                    }
+                }
+
+               
+            }
+            static void Main(string[] args)
+            {
+                int[] integers = { 1, 2,3,4,5};
+
+                SimpleTree BST = new SimpleTree();
+
+                 MakeTreeRecursively(integers,0,integers.Length-1);
+
+                var ReturnPath= PreOrder();
+
+                foreach (var item in ReturnPath)
+                {
+                    Console.Write($"{item}->");
+
+                }
+
+
+
+
+            }
+
+        }
+        
     }
 }
